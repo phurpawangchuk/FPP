eb5d8155cbe53e54a1f643fc9a18652f4f812cb9
Phurpa <phurpawangchuk20@gmail.com>
2024-03-06 14:14:34 -0600

    Code refactored

--- Assignment/Assignment6/Main.java
+++ Assignment/Assignment6/Main.java
@@ -1,6 +1,19 @@
-package Assignment6;/**
+package Assignment6;
+
+/**
  * @author phurpawangchuk
  * @contact phurpa.wangchuk@miu.edu
  * @year 2024-05-03
- */public class Main {
+ */
+public class Main {
+    public static void main(String[] args) {
+        MyTable t = new MyTable();
+        t.add('a', "Andrew");
+        t.add('b',"Billy");
+        t.add('c',"Charlie");
+        t.add('d',"Doji");
+
+        System.out.println(t.get('b'));
+        System.out.println(t);
+    }
 }
--- Assignment/Assignment6/MyTable.java
+++ Assignment/Assignment6/MyTable.java
@@ -1,39 +1,61 @@
-package Assignment6.A6;
+package Assignment6;
 
 /**
  * @author phurpawangchuk
  * @contact phurpa.wangchuk@miu.edu
- * @year 2024-05-03
+ * @year 2024-06-03
  */
 public class MyTable {
     private Entry[] entries;
+    MyTable(){
+        entries = new Entry[26];
+    }
+
     //returns the String that is matched with char c in the table
     public String get(char c){
-//implement
-
+        //implement
+        int index = c -'a';
+        if(index >=0 && index < 26){
+            return entries[index].str;
+        }
         return null;
     }
     //adds to the table a pair (c, s) so that s can be looked up using c
     public void add(char c, String s) {
-        
-//implement
+        //implement
+        int index = c - 'a';
+        if(index >= 0 && index < 26) {
+            entries[index] = new Entry(c, s);
+        }
     }
+
     //returns a String consisting of nicely formatted display
-//of the contents of the table
+    //of the contents of the table
     public String toString() {
-//implement
-        return null;
+        //implement
+        StringBuilder sb = new StringBuilder();
+        for(Entry entry: entries){
+            if(entry != null) {
+                sb.append(entry.toString()).append("\n");
+            }
+        }
+        return sb.toString();
     }
+
     private class Entry {
         char ch;
         String str;
+
         Entry(char ch, String str){
-//implement
+            //implement
+            this.ch = ch;
+            this.str = str;
         }
         //returns a String of the form "ch->str"
         public String toString() {
-//implement
-            return null;
+            //implement
+            return ch+" -> "+str;
         }
     }
+
 }
